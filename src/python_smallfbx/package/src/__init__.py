0e0d80e22f74b2a9644a23cefbb5f0546e6d5004
blurryroots <blurryroots@posteo.de>
2021-03-29 00:23:50 +0200

    bumped python_smallfbx version

--- src/python_smallfbx/package/src/__init__.py
+++ src/python_smallfbx/package/src/__init__.py
@@ -5,5 +5,8 @@ from .python_smallfbx import Document
 
 __all__ = ['Document']
 
+_SmallFBX_Version = '2021.3.16'
+_version = 4
+
 def version():
-	return "2021.3.16"
+	return f'{_SmallFBX_Version}.{_version}'
